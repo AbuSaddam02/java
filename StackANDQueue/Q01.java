8b53a6065b5ef58e0790ee171ae3327bc2378904
JSD <pro.mohammedghaben@gmail.com>
2025-02-22 22:42:04 +0200

    Rename main method to stacky and add Stack1b method for additional stack operations

--- StackANDQueue/Q01.java
+++ StackANDQueue/Q01.java
@@ -3,7 +3,7 @@ package StackANDQueue;
 import java.util.Stack;
 
 public class Q01 {
-    public static void main(String[] args) {
+    public static void stacky() {
         Stack<Character> s1 = new Stack<Character>();
         Stack<Character> s2 = new Stack<Character>();
         s1.push('a');
@@ -14,7 +14,28 @@ public class Q01 {
         s2.push(ch);
         ch = s2.pop();
         s1.push('e');
-        System.out.println(s1);
-        System.out.println(s2);
+        System.out.println("S1 ~>" + s1);
+        System.out.println("S2 ~>" + s2);
+        System.out.println("ch~>" + ch);
+
+    }
+
+    public static void Stack1b() {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        s1.push(1);
+        s2.push(2);
+        s1.push(3);
+        s2.push(4);
+        s1 = s2;
+        s1.push(5);
+        s2.push(6);
+        System.out.println("S1 ~>" + s1);
+        System.out.println("S2 ~>" + s2);
+}
+    public static void main(String[] args) {
+        stacky();
+        System.out.println("===================================\n Saeef B");
+        Stack1b();
     }
 }
