e3009f0d94a7b715ef3dd5a03cdc0d24aefa7ec6
JSD <pro.mohammedghaben@gmail.com>
2025-02-23 00:08:48 +0200

    Update Q2 class to use String type in queue operations and comment out previous integer logic

--- StackANDQueue/Q2.java
+++ StackANDQueue/Q2.java
@@ -7,16 +7,20 @@ public class Q2 {
 
     public static void main(String[] args) {
 
-        Queue<Integer> q1 = new Queue<Integer>();
-        Queue<Integer> q2 = new Queue<Integer>();
-        q1.insert(32);
-        q1.insert(-6);
-        q2.insert(q1.head());
-        q1.insert(q1.remove() + q1.remove());
-        if (q1.isEmpty())
-            q1.insert(9);
-        else
-            q2.insert(17);
+        // Queue<Integer> q1 = new Queue<Integer>();
+        // Queue<Integer> q2 = new Queue<Integer>();
+        // q1.add(32);
+        // q1.add(-6);
+        // q2.add(q1.peek());
+        // q1.add(q1.remove() + q1.remove());
+        // if (q1.isEmpty())
+        //     q1.add(9);
+        // else
+        //     q2.add(17);
 
-    }
+        Queue<String> q1 = new Queue<String>();
+        Queue<String> q2 = new Queue<String>();
+        q1.add("Hello");
+        q1.add("World");
+        q2.add(q1.remove());
 }
